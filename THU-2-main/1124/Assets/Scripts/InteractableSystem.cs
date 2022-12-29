3733868d55da55bb2e3c43afaf831d0380843ea8
Lucien <jean20011116@gmail.com>
2022-12-29 15:56:41 +0800

    1229

--- THU-2-main/1124/Assets/Scripts/InteractableSystem.cs
+++ THU-2-main/1124/Assets/Scripts/InteractableSystem.cs
@@ -17,6 +17,8 @@ namespace LUCIEN
         private GameObject propActive;
         [SerializeField, Header("啟動後的對話資料")]
         private DialogueData dataDialogueActive;
+        [SerializeField, Header("啟動後對話結束後的事件")]
+        private UnityEvent onDialogueFinishAfterActive;
 
         private string nameTarget = "PlayerCapsule";
         private DialogueSystem dialogueSystem;
@@ -43,7 +45,7 @@ namespace LUCIEN
                 }
                 else
                 {
-                    dialogueSystem.StartDialogue(dataDialogueActive);
+                    dialogueSystem.StartDialogue(dataDialogueActive, onDialogueFinishAfterActive);
                 }
                 
                 
