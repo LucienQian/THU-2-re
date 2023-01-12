f83d6df6340f05b17a98634b6d381295183d3a97
Lucien <jean20011116@gmail.com>
2023-01-12 18:53:55 +0800

    0112

---
+++ THU QIMO/Assets/Scripts/DamageSystem.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace LUCIEN
+{
+    /// <summary>
+    /// 傷害系統
+    /// </summary>
+    public class DamageSystem : MonoBehaviour
+    {
+        [SerializeField, Header("會造成傷害的目標")]
+        private string nameTarget;
+        [Header("受傷與爆炸音效")]
+        [SerializeField]
+        private AudioClip soundHit;
+        [SerializeField]
+        private AudioClip soundExplosion;
+
+        // 碰撞開始事件 一次
+        private void OnCollisionEnter(Collision collision)
+        {
+            // print ("碰撞：" + collision.gameObject);
+
+            // 如果 碰到物件的名稱 包含 敵機 就爆炸
+            if (collision.gameObject.CompareTag("Monster"))
+            {
+                Destroy(gameObject);
+            }
+
+        }
+
+        // 碰撞離開事件 一次
+        private void OnCollisionExit(Collision collision)
+        {
+
+        }
+
+        // 碰撞持續事件
+        private void OnCollisionStay(Collision collision)
+        {
+
+        }
+    }
+}
---
+++ THU QIMO/Assets/Scripts/PlayerFinal.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace LUCIEN
+{
+    /// <summary>
+    /// 玩家結束管理：死亡或過關
+    /// </summary>
+    public class PlayerFinal : MonoBehaviour
+    {
+        // 被刪除時會執行一次
+        private void OnDestroy()
+        {
+            // 死亡
+            FinalManager.instance.GameOver("挑戰失敗！");
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Monster"))
+            {
+                 FinalManager.instance.GameOver("挑戰失敗！");
+            }
+        }
+    }
+}
